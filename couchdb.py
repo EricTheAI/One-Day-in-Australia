f7b595990a1d3fb11dc0ab4c21ec8728d9b24ac5
jiaw8 <wangjia9281@gmail.com>
2017-04-18 19:54:24 +1000

    Update couchdb.py

--- couchdb.py
+++ couchdb.py
@@ -9,11 +9,11 @@ with open('tinyTwitter.json', "r") as fp:
         line = fp.readline()
         j=json.loads(line[0:-2])
         id=j["meta"]["id"]
-        r=requests.put("http://130.56.248.231:5985/cloud/"+id,data='{"category":"tinyTwitter","metadata":'+line[0:-2]+'}')
+        r=requests.put("http://130.56.248.231:5984/cloud/"+id,data='{"category":"tinyTwitter","metadata":'+line[0:-2]+'}')
         print r.status_code
         print r.content
 
 # get view
 # tweeter catagory and tweeter text
-r = requests.get("http://130.56.248.231:5985/cloud/_design/view1/_view/view1")
+r = requests.get("http://130.56.248.231:5984/cloud/_design/view1/_view/view1")
 print r.content

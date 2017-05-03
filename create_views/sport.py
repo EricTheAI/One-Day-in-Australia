dd06923181101ee0950e2b9cd397bebf78cb8261
louiseliang <louiseshel0318@gmail.com>
2017-05-03 17:18:30 +1000

    update views code

--- create_views/sport.py
+++ create_views/sport.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
-import couchdb
+import couchdb,sys
 from couchdb.design import ViewDefinition
 
 couch = couchdb.Server('http://admin:admin@130.56.248.231:5984/')
 print couch
-db = couch['cloud2']
+db = couch[sys.argv[1]]
 
 #1.ski
 view = ViewDefinition('sport', 'ski', 
@@ -18,7 +18,7 @@ doc.metadata.json.text.toLowerCase().indexOf('snowboard') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFC2') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFBF') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','ski'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
@@ -33,7 +33,7 @@ doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFCC') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\u26F3') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('flag in hole') != -1
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','golf'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
@@ -46,7 +46,7 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('surf') != -1 || 
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFC4') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','surf'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
@@ -59,7 +59,7 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('swim') != -1 || 
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFCA') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','swim'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
@@ -73,7 +73,7 @@ constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('biking') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83D\uDEB4') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83D\uDEB5') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','bike'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
@@ -86,7 +86,7 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('soccer') != -1 || 
 doc.metadata.json.text.toLowerCase().indexOf('\u26BD') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','soccer'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
@@ -99,12 +99,12 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('baseball') != -1 || 
 doc.metadata.json.text.toLowerCase().indexOf('\u26BE') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','baseball'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#7.basketball
+#8.basketball
 view = ViewDefinition('sport', 'basketball', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
@@ -112,40 +112,39 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('basketball') != -1 || 
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFC0') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','basketball'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#8.volleyball
-view = ViewDefinition('sport', 'basketball', 
+#9.volleyball
+view = ViewDefinition('sport', 'volleyball', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
 doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('volleyball') != -1 || 
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFD0') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','volleyball'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#9.rugby
-view = ViewDefinition('sport', 'rugby', 
+#10.football
+view = ViewDefinition('sport', 'football', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
 doc.metadata.json.place.name == 'Sydney')
-constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('rugby') != -1 || 
-doc.metadata.json.text.toLowerCase().indexOf('american football') != -1  || 
+constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('football') != -1 || 
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFC8') != -1 || 
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFC9') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','football'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#10.tennis
+#11.tennis
 view = ViewDefinition('sport', 'tennis', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
@@ -153,12 +152,12 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('tennis') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFBE') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','tennis'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#11.bowling
+#12.bowling
 view = ViewDefinition('sport', 'bowling', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
@@ -167,12 +166,12 @@ constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('bowling') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('strike') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFB3') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','bowling'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#12.pingpong
+#13.pingpong
 view = ViewDefinition('sport', 'pingpong', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
@@ -180,12 +179,12 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('pingpong') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFD3') != -1
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','pingpong'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#13.badminton
+#14.badminton
 view = ViewDefinition('sport', 'badminton', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
@@ -193,12 +192,12 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('badminton') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFF8') != -1
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','badminton'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#14.boxing
+#15.boxing
 view = ViewDefinition('sport', 'boxing', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
@@ -206,12 +205,12 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('boxing') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83E\uDD4A') != -1
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','boxing'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#15.skate
+#16.skate
 view = ViewDefinition('sport', 'skate', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
@@ -220,12 +219,12 @@ constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('skate') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('skating') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\u26F8') != -1
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','skate'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#16.jogging
+#17.jogging
 view = ViewDefinition('sport', 'jogging', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
@@ -233,19 +232,19 @@ doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('jogging') != -1 ||
 doc.metadata.json.text.toLowerCase().indexOf('\uD83C\uDFC3') != -1 
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','jogging'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
 
-#17.gym
+#18.gym
 view = ViewDefinition('sport', 'gym', 
 '''function(doc) {
 constraint_1 = (doc.metadata.json.place.name == 'Melbourne' || 
 doc.metadata.json.place.name == 'Sydney')
 constraint_2 = doc.metadata.json.text.toLowerCase().indexOf('gym') != -1
 if(constraint_1 && constraint_2) 
-emit([doc.metadata.json.place.name,doc.metadata.json.text], 1); 
+emit([doc.metadata.json.place.name,doc.metadata.json.coordinates.coordinates,doc.metadata.json.created_at,'sport','gym'], 1); 
 }''',
 '''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
 view.sync(db)
---
+++ create_views/stream_sport.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import couchdb,sys
+from couchdb.design import ViewDefinition
+
+couch = couchdb.Server('http://admin:admin@130.56.248.231:5984/')
+print couch
+db = couch[sys.argv[1]]
+print db
+view = ViewDefinition('category', 'sport', 
+'''
+function(doc) {
+constraint_1 = (doc.metadata.place.name == 'Melbourne' || 
+doc.metadata.place.name == 'Sydney')
+
+regex=/(#| |^)(ski|snowboard|skiing|skiied)( |$|s|es|ed)|\u26F7|\uD83C\uDFC2|\uD83C\uDFBF/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'ski',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(golf|flag in hole)( |ing|$|s|es|ed)|\uD83C\uDFCC|\u26F3/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'golf',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(surf)( |ing|$|s|es|ed)|\uD83C\uDFC4/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'surf',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(swim|swimming)( |$|s|es|ed)|\uD83C\uDFCA/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'swim',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(biking|bike|bicycle|bicycling)( |$|s|es|ed|d)|\uD83D\uDEB4|\uD83D\uDEB5/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'bike',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(soccer)( |$|s|es|ed)|\u26BD/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'soccer',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(baseball)( |$|s|es|ed)|\u26BE/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'baseball',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(basketball)( |$|s|es|ed)|\uD83C\uDFC0/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'basketball',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(volleyball)( |$|s|es|ed)|\uD83C\uDFD0/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'volleyball',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(football)( |$|s|es|ed)|\uD83C\uDFC8|\uD83C\uDFC9/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'football',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(tennis)( |$|s|es|ed)|\uD83C\uDFBE/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'tennis',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(bowling)( |$|s|es|ed)|\uD83C\uDFB3/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'bowling',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(pingpong|table tennis)( |$|s|es|ed)|\uD83C\uDFD3/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'pingpong',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(badminton)( |$|s|es|ed)|\uD83C\uDFF8/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'badminton',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(boxing)( |$|s|es|ed)|\uD83E\uDD4A/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'boxing',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(skate|skating)( |$|s|es|ed|d)|\u26F8/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'skate',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(jogging|running|run)( |$|s|es|ed)|\uD83C\uDFC3/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'run',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+
+regex=/(#| |^)(gym)/i
+constraint_2 = doc.metadata.text.match(regex)
+if(constraint_1 && constraint_2) 
+emit([doc.metadata.place.name,'gym',doc._id,doc.metadata.coordinates.coordinates,doc.metadata.created_at], 1); 
+}
+''',
+'''function (keys, values, rereduce) {if (rereduce) {return sum(values);} else {return values.length;}}''')
+view.sync(db)
+
+print 'complete!'
+

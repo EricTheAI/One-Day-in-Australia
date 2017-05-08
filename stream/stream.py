a736063804b254abe81c59132a9afc31a15a5df6
FannieHF <fanniehf@gmail.com>
2017-05-09 00:15:39 +1000

    Update stream.py
    add sentimental polarity into data received by twitter stream

--- stream/stream.py
+++ stream/stream.py
@@ -1,9 +1,7 @@
-
-
-import tweepy
 from tweepy import OAuthHandler
-import json 
+import json,tweepy
 from pip._vendor import requests
+from textblob import TextBlob
 
 
 consumer_key = 'gwSo8FuaZkoRyinoCEEyKDJPi'
@@ -21,7 +19,8 @@ class StreamListener_todb(tweepy.StreamListener):
     def on_status(self, status):
         json_str = json.dumps(status._json)
         id = str(status._json['id'])
-        r=requests.put("http://localhost:5984/stream/"+id,data='{"category":"australia","metadata":'+json_str+'}')
+        blob_text = str(TextBlob(status._json['text']).sentiment.polarity)
+        r=requests.put("http://localhost:5984/stream/"+id,data='{"category":"australia","metadata":'+json_str+',"sentiment":'+blob_text+'}')
 
     def on_error(self, status_code):
         if status_code == 420:
@@ -31,7 +30,3 @@ stream_listener = StreamListener_todb()
 stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
 
 stream.filter(locations=[109.5993213654,-44.55159558,159.3459911346,-11.059461736])
-
-
-
-

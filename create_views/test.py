81527dce00e1b81227c1dae2ff797f907f84ad7c
louiseliang <louiseshel0318@gmail.com>
2017-05-11 06:28:04 +1000

    views update code

--- create_views/test.py
+++
@@ -1,8 +0,0 @@
-import couchdb,sys
-from couchdb.design import ViewDefinition
-
-couch = couchdb.Server('http://admin:admin@localhost:15984/')
-print couch
-db = couch[sys.argv[1]]
-print db
-print 'complete!'
